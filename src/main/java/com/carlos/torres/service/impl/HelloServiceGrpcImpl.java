4447b27fb8ce28194b05d71a37c5daeddbcd76f8
Carlos Torres <carlos.a.torres-gonzalez@capgemini.com>
2019-08-19 14:36:09 +0200

    gRPC hello sever

--- src/main/java/com/carlos/torres/service/impl/HelloServiceGrpcImpl.java
+++ src/main/java/com/carlos/torres/service/impl/HelloServiceGrpcImpl.java
@@ -1,15 +1,22 @@
 package com.carlos.torres.service.impl;
 
+import org.lognet.springboot.grpc.GRpcService;
+
 import com.carlos.torres.model.impl.HelloRequest;
 import com.carlos.torres.model.impl.HelloResponse;
 import com.carlos.torres.service.HelloServiceGrpc.HelloServiceImplBase;
 
 import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
 
+@GRpcService
+@Slf4j
 public class HelloServiceGrpcImpl extends HelloServiceImplBase {
 
 	@Override
 	public final void hello(final HelloRequest request, final StreamObserver<HelloResponse> responseObserver) {
+		
+		log.info("Request received in 'hello' gRPC service...");
 
 		final HelloResponse response = 
 				HelloResponse.newBuilder()
@@ -18,6 +25,8 @@ public class HelloServiceGrpcImpl extends HelloServiceImplBase {
 						.append(request.getFirstName()).append(" ").append(request.getLastName()).toString())
 				.build();
 
+		log.info("Response served");
+		
 		responseObserver.onNext(response);
 		responseObserver.onCompleted();
 	}
